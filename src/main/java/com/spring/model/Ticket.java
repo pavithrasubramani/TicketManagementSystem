27c39de67d4ea0602fb8d3f0ce991737623b4485
PavithraSubramani <pavithra.subramani@revature.com>
2018-11-20 11:22:59 +0530

    SpringHibernet task changes

--- src/main/java/com/spring/model/Ticket.java
+++ src/main/java/com/spring/model/Ticket.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="ticket")
@@ -30,7 +33,7 @@ public class Ticket {
 	@JoinColumn(name = "s_id", nullable = false)
 	private Student s;
 	
-    @OneToMany(cascade=CascadeType.ALL)
+    @OneToMany(mappedBy="tk", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
 	private List<TicketHistory> ticketHistory;
 	 
     
